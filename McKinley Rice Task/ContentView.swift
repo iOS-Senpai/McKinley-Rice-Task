13bdfb074995f9c6bdc2dd291fe5d5a2d9018b45
Mukku Pradeep Kumar <mukkupradeep.kumar@absa.africa>
2023-07-17 13:42:39 +0530

    Initial Commit

---
+++ McKinley Rice Task/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  McKinley Rice Task
+//
+//  Created by AB020QU on 2023/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ McKinley Rice Task/McKinley_Rice_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  McKinley_Rice_TaskApp.swift
+//  McKinley Rice Task
+//
+//  Created by AB020QU on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct McKinley_Rice_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
